b76161c73b3e7c48d687c5398642a9e45c956dc8
mekxl <145471587+mekxl@users.noreply.github.com>
2025-07-19 19:09:29 -0300

    Create palavras.js

---
+++ palavras.js
@@ -0,0 +1,40 @@
+const listaPalavras = [
+  "cuzao", "penis", "bolas", "vadio", "pinto", "gosto", "surra", "tanga", "cuspe", "dildo", "porno", "malha",
+  "nudes", "bosta", "merda", "chupe", "samba", "roubo", "vulva", "gozar", "guria", "corno", "treta", "doido",
+  "viado", "putas", "babao", "bunda", "leite", "mamas", "perna", "tetas", "putao", "papos", "mansa", "dedao",
+  "rosca", "mamou", "mamam", "leito", "orgia", "rolao", "socar", "lambe", "tesao", "molha", "cevas", "chuca",
+  "pariu", "comer", "comia", "cuzin", "beijo", "pauza", "melao", "cagar", "gorda", "farto", "gemer", "coito",
+  "risos", "macho", "tonta", "bocas", "preto", "louro", "louco", "matar", "morre", "ferir", "braba", "burra",
+  "putin", "comem", "feios", "puxao", "puxam", "porra", "carai", "foder", "fudeu", "meter", "furar",
+
+  "arroz", "carro", "pedra", "luzia", "lapis", "cobra", "zebra", "bicho", "livro", "nuvem", "chuva", "parda",
+  "ruiva", "amora", "selva", "turbo", "cacto", "lindo", "burro", "massa", "tocha", "vinil", "meias", "sushi", "trote", "cofre", "vinho", "pirar", "manja", "vazar", "ranho", "larva", "sagaz", "duros", "densa",
+  "mofar", "troco", "velha", "berro", "urubu", "muros", "vetor", "moeda", "raiva", "zumbi", "xingo", "nobre",
+  "rolou", "sadia", "nossa", "lenta", "noite", "vazio", "dores", "cinto", "manco", "velho", "sogra",
+  "noiva", "hiena", "carne", "muito", "verbo", "nariz", "morro", "pavio", "rouba", "penso", "dessa",
+  "banho", "feira", "salve", "forno", "festa", "notar", "quero", "prato", "roupa", "grade",
+  "canal", "troca", "ganha", "canto", "corte", "dente", "gente", "jovem", "mundo", "piano", "sabor", "tarde",
+  "viver", "fugir", "fazer", "pagar", "saber", "dolar", "ficha", "gordo", "hobby", "jogar", "lacre",
+  "ninja", "pasta", "quase", "sorte", "tinta", "vapor", "banda", "carta",
+
+  "abalo", "abate", "abduz", "abril", "abrir", "abriu", "abuso", "acaba", "acabe", "acabo",
+  "achei", "acida", "acido", "acima", "acnes", "acres", "acude",
+
+  "tigre", "ursos", "gatos", "porco", "cabra", "patos", "ganso", "cisne", "aguia", "corvo",
+  "pomba", "sabia", "arara", "beija", "cobra", "polvo", "lulas",
+
+  "azuis", "verde", "preto", "roxos", "cinza", "bronze", "branco",
+
+  "pizza", "massa", "peixe", "porco", "leite", "bolos", "torta", "doces", "manga",
+  "melao", "limao", "figos", "milho", "aveia", "trigo",
+
+  "copos", "facas", "forno", "ferro", "pente", "sabao", "creme", "mouse", "radio",
+  "pilha", "placa", "fonte",
+
+  "andar", "pular", "nadar", "subir", "abrir", "ligar", "falar", "olhar", "sentir", "tocar", "pensar",
+  "viver", "morrer", "nascer",
+
+  "raiva", "morte", "tempo", "forca", "poder", "calma", "chave", "chuva", "chama", "chega", "cheia", "china", "choca", "chora",
+  "malha", "velha", "folha", "molha", "bolha", "falha", "calha",
+  "linha", "minha", "tinha", "senha", "lenha", "venha", "tenha", "ninho", "ombro", "braço", "pulso", "peito", "bunda", "nariz", "barba", "feliz", "bravo", "calmo", "livre", "preso"
+];
